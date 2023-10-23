d235e1233e3545e5a273b715212913f51fb22ae1
Caro J-son <125191312+caro-json@users.noreply.github.com>
2023-10-23 15:55:50 +0200

    Update project.cs

--- project.cs
+++ project.cs
@@ -1,1 +1,19 @@
+public class Book
+{
+    public string Title { get; set; }
+}
 
+public class Program
+{
+    public static void Main()
+    {
+        // Skapa en instans av Book med default-konstruktorn
+        Book book = new Book();
+
+        // Sätt egenskapen Title
+        book.Title = "The Great Gatsby";
+
+        // Nu har du en instans av Book med Title satt
+        Console.WriteLine($"Bokens titel är: {book.Title}");
+    }
+}
