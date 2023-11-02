1ea36a921a0cd671686132143d8b5f75d0d7ead8
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-02 11:41:02 +0100

    lagt till bookningsmetod i main

--- project.cs
+++ project.cs
@@ -6,12 +6,28 @@ namespace hotelcsharp
     {
         public static void Main()
         {
-           GuestList guestList = new GuestList();
-           guestList.PrintGuestList(); 
+            GuestList guestList = new GuestList();
+            RoomList roomList = new RoomList();
 
-           RoomList roomList = new RoomList();
-           roomList.PrintRoomList();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Ange numret på det rum du vill boka [1-3]\n");
+            Console.ResetColor();
+
+            roomList.ListAvailableRooms();
+            string input = Console.ReadLine();
+            
+            bool Available = int.TryParse(input, out int roomIndex);
+
+            if (Available && roomIndex > 0 && roomIndex <= roomList.room.Count)
+            {
+                roomList.BookRoom(roomIndex);
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt val. Försök igen.");
+            }
+                
         }
     }
 }
-//Test till Commit
+
