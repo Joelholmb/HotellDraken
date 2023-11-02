cc0fe8fdd5fe6b00b296f5d4539be97aba6320fa
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-02 11:36:46 +0100

    lagt till en boolfunktion + fler egenskaper

--- Rooms.cs
+++ Rooms.cs
@@ -7,16 +7,23 @@ namespace hotelcsharp
         public string RoomName { get; set; }
         public string RoomType { get; set; }
         public int RoomNumber { get; set; }
-
+        public string TypeBed { get; set; }
         public string RoomPrice { get; set; }
+        public string RoomSize { get; set; }
+        public string RoomView {get; set; }
+        public bool IsBooked { get; set; }
         
 
-        public Rooms(string roomname, string roomtype, int roomnumber, string roomprice)
+        public Rooms(string roomname, string roomtype, int roomnumber,string typebed, string roomsize, string roomview, string roomprice)
         {
             RoomName = roomname;
             RoomType = roomtype;
             RoomNumber = roomnumber;
             RoomPrice = roomprice;
+            TypeBed = typebed;
+            RoomSize = roomsize;
+            RoomView = roomview;
+            IsBooked = false;
             
         }
         
