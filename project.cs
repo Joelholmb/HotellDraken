dbabdf425f17d1ac67792ac08a84cfde4865412f
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-06 11:10:09 +0100

    utökat metod i main vid val av rum

--- project.cs
+++ project.cs
@@ -9,23 +9,39 @@ namespace hotelcsharp
             GuestList guestList = new GuestList();
             RoomList roomList = new RoomList();
 
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Ange numret på det rum du vill boka [1-3]\n");
-            Console.ResetColor();
-
-            roomList.ListAvailableRooms();
-            string input = Console.ReadLine();
-            
-            bool Available = int.TryParse(input, out int roomIndex);
-
-            if (Available && roomIndex > 0 && roomIndex <= roomList.room.Count)
+            while(true)
             {
-                roomList.BookRoom(roomIndex);
-            }
-            else
-            {
-                Console.WriteLine("Ogiltigt val. Försök igen.");
-            }
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Ange numret på det rum du vill boka [1-3]\n");
+                Console.ResetColor();
+
+                roomList.ListAvailableRooms();
+                string userinput = Console.ReadLine();
+                
+                bool Available = int.TryParse(userinput, out int roomIndex);
+
+                if (Available && roomIndex > 0 && roomIndex <= roomList.room.Count)
+                {
+                    roomList.ShowInfoRoom(roomIndex);
+                    Console.Write("\nVill du boka detta rum? (ja/nej): ");
+                    string userInput = Console.ReadLine();
+                    
+                    if (userInput == "ja")
+                    {
+                        roomList.BookRoom(roomIndex);
+                        break;
+                    }
+                }
+                else if (Available && roomIndex == 0)
+                {
+                    break; 
+                }
+                else
+                {
+                    Console.WriteLine("Ogiltigt val. Försök igen.");
+                }
+            }     
                 
         }
     }
