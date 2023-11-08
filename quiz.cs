3b3bcba627d365d1b0cd55b50f2de948c62dcf64
Caro J-son <125191312+caro-json@users.noreply.github.com>
2023-11-08 09:40:32 +0100

    Update quiz.cs

--- quiz.cs
+++ quiz.cs
@@ -1,10 +1,43 @@
-using System;
-
-namespace hotelcsharp
+public class MyClass
 {
-    internal class Program
+    public static void Main()
     {
-        static void quiz(string[] args)
+        {
+            System.Console.WriteLine("[1] Svar på frågorna om hotellet");
+            System.Console.WriteLine("[2] Skriv frågor");
+            System.Console.WriteLine("[3] Orkar inte skriva frågor. Jag lämnar över till AI");
 
+            string val = Console.ReadLine();
+
+            switch (val)
+            {
+                case "1":
+
+                    System.Console.WriteLine("");
+                    string topic = Console.ReadLine();
+
+                    System.Console.WriteLine("");
+                    int number = int.Parse(Console.ReadLine());
+
+                    break;
+
+                case "2":
+
+                    System.Console.WriteLine("Vad är frågan, SKRIV IN");
+                    string text = Console.ReadLine();
+
+                    System.Console.WriteLine("Vad är svaret, SKRIV IN");
+                    string svar = Console.ReadLine();
+
+                    System.Console.WriteLine("Vad är frågan, SKRIV IN");
+                    string text2 = Console.ReadLine();
+
+                    System.Console.WriteLine("Vad är svaret, SKRIV IN");
+                    string svar2 = Console.ReadLine();
+
+                    break;
+            }
+        }
     }
 }
+
