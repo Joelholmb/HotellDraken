cca5d096d6df1eb31cd58b2c0d20a710f25a4617
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-13 12:21:17 +0100

    nya funktioner och refaktorering

--- RoomList.cs
+++ RoomList.cs
@@ -1,37 +1,54 @@
-
 namespace hotelcsharp
 {
     public class RoomList
     {
-        public List<Rooms> room = new List<Rooms>
+        private static RoomList instance;
+        public List<Rooms> rooms { get; private set; }
+        private RoomList() 
         {
-        new Rooms("Lancelot", "Superior-rum", 13,"2 enkelsängar", "26 kvadratmeter","en betongvägg", "3700 kr"),
-        new Rooms("Merlin", "Premium-rum",22, "1 queen-size säng", "36 kvadratmeter","staden", "5000 kr"),
-        new Rooms("Arthur", "Svit", 1, "1 kingsize-säng", "49 kvadratmeter","havet", "12000 kr"),
-        };
-        
-            
+            rooms = new List<Rooms>
+            {
+                new Rooms("Lancelot", "Superior-rum", "2 enkelsängar", "26 kvadratmeter", "en betongvägg", "3700 kr"),
+                new Rooms("Merlin", "Premium-rum", "1 queen-size säng", "36 kvadratmeter", "staden", "5000 kr"),
+                new Rooms("Arthur", "Svit", "1 kingsize-säng", "49 kvadratmeter", "havet", "12000 kr"),
+            };
+        }
+
+        public static RoomList GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RoomList();
+            }
+            return instance;
+        }
         
         public void ListAvailableRooms()
         {
-           int index = 1;
-           foreach (var currentroom in room)
-           {
-              if (currentroom.IsBooked == false)
-              {
-                 Console.WriteLine($"{index}. {currentroom.RoomName}, {currentroom.RoomType}.\n   Pris för en natt {currentroom.RoomPrice}.\n");
-              }
-              index++;
-           }
+            int index = 1;
+            foreach (var currentRoom in rooms)
+            {
+                if (currentRoom.IsBooked == false)
+                {
+                    Console.WriteLine($"{index}. {currentRoom.RoomName}, {currentRoom.RoomType}. Pris för en natt {currentRoom.RoomPrice}.");
+                }
+                index++;
+            }
         }
 
         public void BookRoom(int roomIndex)
         {
-            
-            if (roomIndex > 0 && roomIndex <= room.Count && room[roomIndex - 1].IsBooked == false)
+            if (roomIndex > 0 && roomIndex <= rooms.Count)
             {
-                room[roomIndex - 1].IsBooked = true;
-                Console.WriteLine($"Rummet {room[roomIndex - 1].RoomName} har bokats.");
+                Rooms roomToBook = rooms[roomIndex - 1];
+                if (!roomToBook.IsBooked)
+                {
+                    roomToBook.Book();
+                }
+                else
+                {
+                    Console.WriteLine("Rummet är redan bokat.");
+                }
             }
             else
             {
@@ -41,15 +58,45 @@ namespace hotelcsharp
 
         public void ShowInfoRoom(int roomIndex)
         {
-            var selectedRoom = room[roomIndex - 1];
-            Console.WriteLine($"\nDu har valt rummet {selectedRoom.RoomName}");
-            Console.WriteLine($"\n{selectedRoom.RoomType}");
-            Console.WriteLine($"{selectedRoom.RoomSize}");
-            Console.WriteLine($"{selectedRoom.TypeBed}");
-            Console.WriteLine($"Utsikt över {selectedRoom.RoomView}");
-            Console.WriteLine($"Pris per natt: {selectedRoom.RoomPrice}");
+            if (roomIndex > 0 && roomIndex <= rooms.Count)
+            {
+                var selectedRoom = rooms[roomIndex - 1];
+                Console.WriteLine($"\nInformation för rummet {selectedRoom.RoomName}:");
+                Console.WriteLine($"{selectedRoom.RoomType}");
+                Console.WriteLine($"{selectedRoom.RoomSize}");
+                Console.WriteLine($"{selectedRoom.TypeBed}");
+                Console.WriteLine($"Utsikt över {selectedRoom.RoomView}");
+                Console.WriteLine($"Pris per natt: {selectedRoom.RoomPrice}");
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt rumindex.");
+            }
         }
-   }
 
+        public void ListBookedRooms()
+        {
+            Console.WriteLine("Bokade rum:");
+            foreach (var currentRoom in rooms)
+            {
+                if (currentRoom.IsBooked)
+                {
+                    Console.WriteLine($"Bokningsnr: {currentRoom.BookingNumber} - {currentRoom.RoomName}");
+                }
+            }
+        }
 
+        public bool CancelRoomBooking(int bookingNumber)
+        {
+            foreach (var currentRoom in rooms)
+            {
+                if (currentRoom.BookingNumber == bookingNumber && currentRoom.IsBooked)
+                {
+                    currentRoom.CancelBooking();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
