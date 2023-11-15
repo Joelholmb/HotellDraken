c689d9aa2bc165780b8fa12c619e89b826bccb02
Caro J-son <125191312+caro-json@users.noreply.github.com>
2023-11-15 14:44:21 +0100

    Update review.cs

--- review.cs
+++ review.cs
@@ -1,13 +1,14 @@
 
 
-class Review
+class Review1
 {
 
-    static void ReviewUI()
+
+    static void MenuUI()
     {
         Console.WriteLine("1. Multiple choice");
         Console.WriteLine("2. Free text");
-        Console.WriteLine("3. Number choice 1-10");
+
 
     }
 
@@ -33,6 +34,84 @@ class Review
         Review.Add(a);
     }
 }
+
+public class Review1
+ bool keepRunning = true;
+
+while (keepRunning)
+{
+    Console.Clear();
+    MenuUI();
+
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            foreach (Review a in Reviews)
+            {
+                A.ReviewUI();
+            }
+            break;
+        case "2":
+            Console.Clear();
+            AddReviewUI();
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+
+                MultiChoice newReview = new MultiChoice("", 0, "", "", "", "", 0); newReview.AddReview(Reviews);
+            }
+            else if (choice == 2)
+            {
+
+                FreeText newReview = new FreeText("", 0, "", ""); newReview.AddReview(Reviews);
+            }
+            else
+            {
+                System.Console.WriteLine("Wrong input!");
+
+            }
+            break;
+
+    }
+}
+class MultiChoice : Review2
+{
+
+    public MultiChoice(string question, int 1, int 2, int 3, int 4, int 5, int Answer) : base(question)
+    {
+        1 = 1
+            2 = 2
+            3 = 3
+            4 = 4
+            5 = 5
+            Answer = Answer;
+    }
+
+    class FreeText : Review
+    {
+        public string Answer { get; set; }
+
+        public FreeText(string question, string Answer) : base(question)
+        {
+
+            Answer = Answer;
+        }
+
+        public state1 void ReviewUI()
+        {
+            base.ReviewUI();
+        }
+
+    }
+
+}
+
+
+
+
+
 
 
 
