e1fb60aeef823a1a4bc9206050226bc93dddc641
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-15 20:39:55 +0100

    gjort lite tydligare och kompaktare

--- hotelManagement.cs
+++ hotelManagement.cs
@@ -1,93 +1,61 @@
 namespace hotelcsharp
 {
-
     public class HotelManagement
     {
-        private RoomList roomList;
-        public HotelManagement(RoomList roomList)
-    {
-        this.roomList = roomList;
-    }
-        // En strängvariabel för att lagra användarens roll (gäst eller anställd)
-        public string userRole;
+        private MenuEmployee menuEmployee;
+        private Menu menuGuest;
 
-        // Metoden ChooseRole låter användaren välja sin roll i systemet
-        public void ChooseRole()
+        public HotelManagement()
         {
-            Console.WriteLine("Är du en gäst eller en anställd? (G/A)");
-            string input = Console.ReadLine();
+            menuEmployee = new MenuEmployee();
+            menuGuest = new Menu();
+        }
 
-            // Kontrollerar om användaren angav att de är en gäst
-            if (input.Equals("G", StringComparison.OrdinalIgnoreCase))
+        public void Start()
+        {
+            while (true)
             {
-                userRole = "Gäst";
-                ManageGuest();
-                ShowGuestMenu();
+                Console.WriteLine("Är du en gäst eller en anställd? (G/A)\nTryck på 'Q' för att avsluta.");
+                string input = Console.ReadLine()?.ToUpper() ?? "";
+
+                switch (input)
+                {
+                    case "G":
+                        ChooseGuestProfile();
+                        menuGuest.ShowGuestMenu();
+                        break;
+                    case "A":
+                        menuEmployee.ShowEmployeeMenu();
+                        break;
+                    case "Q":
+                        Console.WriteLine("Avslutar programmet...");
+                        return;
+                    default:
+                        Console.WriteLine("Felaktig inmatning. Välj antingen G, A eller Q.");
+                        break;
+                }
             }
-            // Kontrollerar om användaren angav att de är en anställd
-            else if (input.Equals("A", StringComparison.OrdinalIgnoreCase))
+        }
+
+        private void ChooseGuestProfile()
+        {
+            Console.WriteLine("Välj vilken gästprofil du vill logga in med:");
+            for (int i = 0; i < GuestList.guests.Count; i++)
             {
-                userRole = "Anställd";
-                ManageEmployee();
-                ShowEmployeeMenu();
+                Console.WriteLine($"{i + 1}. {GuestList.guests[i].Name}");
+            }
+
+            int selectedGuestIndex;
+            if (int.TryParse(Console.ReadLine(), out selectedGuestIndex) &&
+                selectedGuestIndex > 0 && selectedGuestIndex <= GuestList.guests.Count)
+            {
+                Guest chosenGuest = GuestList.guests[selectedGuestIndex - 1];
+                Console.WriteLine($"Välkommen {chosenGuest.Name}.");
             }
             else
             {
-                Console.WriteLine("Felaktig inmatning. Välj antingen G eller A.");
-                ChooseRole();
+                Console.WriteLine("Felaktig inmatning. Välj ett nummer från listan.");
             }
-        }
-        // Visar menyn för gäster när man väljer G.
-        private void ShowGuestMenu()
-        {
-            Menu guestMenu = new Menu(roomList);
-            guestMenu.ShowGuestMenu();
-            
-        }
-        //Låter en logga in med en gästprofil
-        private void ManageGuest()
-        {
-            Console.WriteLine("Välj vilken profil du vill logga in med:");
-            // Visar en lista över alla gäster
-            for (int i = 0; i < GuestList.guests.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {GuestList.guests[i]}");
-            }
-            
-             // Läser in användarens val och kontrollerar om det är ett giltigt index
-            if (int.TryParse(Console.ReadLine(), out int selectedGuestIndex))
-            {
-                // Justerar indexet till 1 då det börjar från 0 annars.
-                selectedGuestIndex -= 1;
-                // Kontrollerar att det valda indexet är inom giltiga gränser
-                if (selectedGuestIndex >= 0 && selectedGuestIndex < GuestList.guests.Count)
-                {
-                    Guest chosenGuest = GuestList.guests[selectedGuestIndex];
-                    Console.WriteLine($"Välkommen {chosenGuest.Name}. Vad vill du göra?");
-                    
-                }
-                else
-                {
-                    Console.WriteLine("Felaktig inmatning. Välj ett nummer från listan.");
-                    ManageGuest();
-                }
-            }
-  
-        }
-
-        private void ShowEmployeeMenu()
-        {
-            MenuEmployee employeeMenu = new MenuEmployee(roomList);
-            employeeMenu.ShowEmployeeMenu();
-            
-        }
-
-
-        private void ManageEmployee()
-        {
-            
-            Console.WriteLine("Välkommen tillbaka, kollega!");
-            
         }
     }
 }
