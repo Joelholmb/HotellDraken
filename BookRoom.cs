74541afc1116f65a0cfe775a897376b6a8d74c68
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-16 00:18:20 +0100

    förtydliga kod

--- BookRoom.cs
+++ BookRoom.cs
@@ -10,57 +10,73 @@ namespace hotelcsharp
 
             RoomList.ListAvailableRooms();
 
+            int roomIndex = GetRoomChoice();
+            if (roomIndex == 0) 
+            {
+                Console.WriteLine("Bokning avbruten.");
+                return;
+            }
+
+            RoomList.ShowInfoRoom(roomIndex);
+            if (GetUserConfirmation())
+            {
+                ProcessBooking(roomIndex);
+            }
+        }
+
+        private int GetRoomChoice()
+        {
             while (true)
             {
                 Console.WriteLine("\nAnge numret på det rum du vill boka [1-3] eller 0 för att avbryta:");
-                string userInput = Console.ReadLine();
-
-                if (int.TryParse(userInput, out int roomIndex) && roomIndex > 0 && roomIndex <= RoomList.rooms.Count)
+                if (int.TryParse(Console.ReadLine(), out int roomIndex) && 
+                    (roomIndex == 0 || (roomIndex > 0 && roomIndex <= RoomList.rooms.Count)))
                 {
-                    RoomList.ShowInfoRoom(roomIndex);
-                    Console.Write("\nVill du boka detta rum? (ja/nej): ");
-                    userInput = Console.ReadLine();
-                    
-                    if (userInput.Equals("ja", StringComparison.OrdinalIgnoreCase))
-                    {
-                        var availableIntervals = RoomList.GetAvailableWeekIntervals(roomIndex, 4);
-                        Console.WriteLine("Välj ett bokningsintervall från följande tillgängliga tider:");
-                        for (int i = 0; i < availableIntervals.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1}. {availableIntervals[i].Item1.ToShortDateString()} till {availableIntervals[i].Item2.ToShortDateString()}");
-                        }
-
-                        Console.WriteLine("Välj ett intervall genom att ange numret:");
-                        int intervalChoice;
-                        while (!int.TryParse(Console.ReadLine(), out intervalChoice) || intervalChoice < 1 || intervalChoice > availableIntervals.Count)
-                        {
-                            Console.WriteLine("Ogiltigt val, försök igen:");
-                        }
-
-                        var chosenInterval = availableIntervals[intervalChoice - 1];
-                        bool bookingSuccess = RoomList.BookRoom(roomIndex, chosenInterval.Item1, chosenInterval.Item2);
-                        if (bookingSuccess)
-                        {
-                            Console.WriteLine($"Rum {RoomList.rooms[roomIndex - 1].RoomName} bokat från {chosenInterval.Item1.ToShortDateString()} till {chosenInterval.Item2.ToShortDateString()}.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Bokningen kunde inte genomföras.");
-                        }
-                        break;
-                    }
+                    return roomIndex;
                 }
-                else if (roomIndex == 0)
-                {
-                    Console.WriteLine("Bokning avbruten.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Ogiltigt val. Försök igen.");
-                }
+                Console.WriteLine("Ogiltigt val. Försök igen.");
             }
+        }
+
+        private bool GetUserConfirmation()
+        {
+            Console.Write("\nVill du boka detta rum? (ja/nej): ");
+            return Console.ReadLine().Equals("ja", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private void ProcessBooking(int roomIndex)
+        {
+            var availableIntervals = RoomList.GetAvailableWeekIntervals(roomIndex, 4);
+            Console.WriteLine("\nVälj ett bokningsintervall från följande tillgängliga tider:");
+            for (int i = 0; i < availableIntervals.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {availableIntervals[i].Item1.ToShortDateString()} till {availableIntervals[i].Item2.ToShortDateString()}");
+            }
+
+            int intervalChoice = GetIntervalChoice(availableIntervals.Count);
+            var chosenInterval = availableIntervals[intervalChoice - 1];
+
+            if (RoomList.BookRoom(roomIndex, chosenInterval.Item1, chosenInterval.Item2))
+            {
+                Console.WriteLine($"Rum {RoomList.rooms[roomIndex - 1].RoomName} bokat från {chosenInterval.Item1.ToShortDateString()} till {chosenInterval.Item2.ToShortDateString()}.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Bokningen kunde inte genomföras.");
+            }
+        }
+
+        private int GetIntervalChoice(int count)
+        {
+            int intervalChoice;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out intervalChoice) && intervalChoice >= 1 && intervalChoice <= count)
+                {
+                    return intervalChoice;
+                }
+            } 
         }
     }
 }
-
