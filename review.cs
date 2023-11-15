532cebdb57925d1728c944545870c368af0b86da
Caro J-son <125191312+caro-json@users.noreply.github.com>
2023-11-15 15:45:41 +0100

    Update review.cs

--- review.cs
+++ review.cs
@@ -1,59 +1,109 @@
-class Review.program
+
+
+class Review
 {
-   static void Review1()
-        bool keepRunning = true;
+
+
+    static void MenuUI()
+    {
+        Console.WriteLine("1. Multiple choice");
+        Console.WriteLine("2. Free text");
+
+
+    }
+
+    static void Review()
+    {
+
+        List<Review> Reviews = new List<Review>();
+
+
+        Review a;
+        a = new MultiChoice("", 100, "1", "2", "3", "4", "5");
+        Review.Add(a);
+
+        a = new FreeText("What can my company do to better serve your needs?", "");
+        Review.Add(a);
+        a = new FreeText("How satisfied are you with our products/services?", "");
+        Review.Add(a);
+        a = new FreeText("What value do we provide?", "");
+        Review.Add(a);
+        a = new FreeText("What do you like our hotel?", "");
+        Review.Add(a);
+        a = new FreeText("How is your stay?", "");
+        Review.Add(a);
+    }
+
+
+
+
+    bool keepRunning = true;
 
         while (keepRunning)
+    {
+        Console.Clear();
+
+        string input = Console.ReadLine();
+
+        switch (input)
         {
-            Console.Clear();
-            Console.WriteLine("1. Multiple choice");
-            Console.WriteLine("2. Free text");
-
-            string input = Console.ReadLine();
-
-            switch (input)
-            {
                 case "1":
-                    foreach (Review review in Reviews)
+                    foreach (Review a in Reviews)
                     {
-                        review.ReviewUI();
+                        A.ReviewUI();
                     }
-                    break;
-
+break;
                 case "2":
-                    int choice = int.Parse(Console.ReadLine());
-
-                    if (choice == 1)
-                    {
-                        MultiChoice newReview = new MultiChoice("", 1, 2, 3, 4, 5, 0);
-                        newReview.AddReview(Reviews);
-                    }
-                    else if (choice == 2)
-                    {
-                        FreeText newReview = new FreeText("", "");
-                        newReview.AddReview(Reviews);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Wrong input!");
-                    }
-                    break;
-            }
-        }
-    }
-
-    class Review
+    int choice = int.Parse(Console.ReadLine());
+    if (choice = 1)
     {
-        // Your Review class implementation...
-    }
 
-    class MultiChoice : Review
-    {
-        // Your MultiChoice class implementation...
+        MultiChoice newReview = new MultiChoice("", 0, "", "", "", "", 0); newReview.AddReview(Reviews);
     }
-
-    class FreeText : Review
+    else if (choice = 2)
     {
-        // Your FreeText class implementation...
+
+        FreeText newReview = new FreeText("", 0, "", ""); newReview.AddReview(Reviews);
+    }
+    else
+    {
+        System.Console.WriteLine("Wrong input!");
+
+    }
+    break;
+
+}
+
+public MultiChoice(string question, int 1, int 2, int 3, int 4, int 5, int Answer) : base(question)
+        {
+    {
+        1 = 1
+                2 = 2
+                3 = 3
+                4 = 4
+                5 = 5
+                Answer = Answer;
     }
 }
+
+public FreeText(string question, string Answer) : base(question)
+        {
+
+    Answer = Answer;
+}
+
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
