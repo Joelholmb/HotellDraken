523ff83435f77b1a4c1eb9f7e50abd71d7cd8de0
Caro J-son <125191312+caro-json@users.noreply.github.com>
2023-11-15 18:54:47 +0100

    Update HotelReview.cs

--- HotelReview.cs
+++ HotelReview.cs
@@ -1,6 +1,6 @@
-public class GuestReview
+public class Review1
 {
-    Public void R1()
+Public void R1()
     {
         Console.WriteLine("1. Multiple choice");
         Console.WriteLine("2. Free text");
@@ -8,7 +8,7 @@ public class GuestReview
     }
 
 
-    class MultiChoice : GuestReview
+    class MultiChoice : Review1
     {
         public int Option1 { get; set; }
         public int Option2 { get; set; }
@@ -27,7 +27,7 @@ public class GuestReview
             Answer = answer;
         }
 
-        public void ReviewUI()
+        public void Review1UI()
         {
 
             Console.WriteLine($"Options: {Option1}, {Option2}, {Option3}, {Option4}, {Option5}");
@@ -36,15 +36,48 @@ public class GuestReview
         }
     }
 
-    static void Review()
+    static void Review1UI()
     {
-        List<GuestReview> Reviews = new List<GuestReview>();
+        // Declare the list outside the method
+        List<Review1> Reviews = new List<Review1>();
 
-        // Creating a new instance of MultiChoice
-        GuestReview a = new MultiChoice("MultiChoice Question", 1, 2, 3, 4, 5, 3);
+        // Instantiate the objects and add them to the list
+        Review1 a = new MultiChoice("MultiChoice Question", 1, 2, 3, 4, 5, 3);
+        Reviews.Add(a);
+        a = new MultiChoice("MultiChoice Question", 1, 2, 3, 4, 5, 3);
+        Reviews.Add(a);
+        a = new MultiChoice("MultiChoice Question", 1, 2, 3, 4, 5, 3);
         Reviews.Add(a);
 
+        FreeText freeText1 = new FreeText("What can my company do to better serve your needs?", "Answer 1");
+        Reviews.Add(freeText1);
 
+        FreeText freeText2 = new FreeText("How satisfied are you with our products/services?", "Answer 2");
+        Reviews.Add(freeText2);
+
+        FreeText freeText3 = new FreeText("What value do we provide?", "Answer 3");
+        Reviews.Add(freeText3);
+
+        FreeText freeText4 = new FreeText("What do you like our hotel?", "Answer 4");
+        Reviews.Add(freeText4);
+
+        FreeText freeText5 = new FreeText("How is your stay?", "Answer 5");
+        Reviews.Add(freeText5);
     }
+
+    class FreeText : Review1
+    {
+        public string Answer { get; set; }
+
+        public FreeText(string question, string answer)
+        {
+            Answer = answer;
+        }
+    }
+}
+
+
+
+
 
 }
