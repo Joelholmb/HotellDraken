74cb30d40105a6b8e4a52a1b238e97d4e85b622d
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-15 20:40:09 +0100

    onödig kod

--- ManageBooking.cs
+++ ManageBooking.cs
@@ -2,14 +2,6 @@ namespace hotelcsharp
 { 
     class ManageBooking
     {   
-        private RoomList roomList;
-
-        public ManageBooking(RoomList roomList)
-        {
-            this.roomList = roomList;
-        }
-
-
         public void CancelBooking()
         {
             //kontrollerar om bokningen har avbokats
@@ -21,7 +13,7 @@ namespace hotelcsharp
                 Console.WriteLine("Ange bokningsnummer för det rum du vill avboka (eller 0 för att avsluta):");
                 Console.ResetColor();
 
-                roomList.ListBookedRooms();
+                RoomList.ListBookedRooms();
                 //Läser in användarens inmatning som en sträng även fast man skriver ett heltal eller sträng eller någon annan typ av data.
                 string userInput = Console.ReadLine();
                 // Försöker konvertera inmatningen till en integer(int)
@@ -32,7 +24,7 @@ namespace hotelcsharp
                         break;
                     }
                     // Försöker avboka rummet och kontrollerar om det lyckades
-                    if (roomList.CancelRoomBooking(bookingNumber))
+                    if (RoomList.CancelRoomBooking(bookingNumber))
                     {
                         Console.WriteLine($"Bokning av rum {bookingNumber} har avbokats.");
                         isCancelled = true;  // Uppdaterar den booleanska flaggan till true för att avsluta loopen
