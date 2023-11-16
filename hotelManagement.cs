a196bd572f405896b6b75c861914e1d3579d34db
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-16 13:06:21 +0100

    korrigerad start i main

--- hotelManagement.cs
+++ hotelManagement.cs
@@ -3,35 +3,6 @@ namespace hotelcsharp
 {
     public static class HotelManagement
     {
-       
-        // Startmetoden som kör huvudmenyn för programmet
-        public static void Start()
-        {
-            while (true)
-            {
-                Console.WriteLine("\nÄr du en gäst eller en anställd? (G/A)\nTryck på 'Q' för att avsluta.");
-                string input = Console.ReadLine()?.ToUpper() ?? "";
-
-                // Hanterar användarens val och navigerar till rätt meny
-                switch (input)
-                {
-                    case "G":
-                        ChooseGuestProfile();
-                        Menu.ShowGuestMenu();
-                        break;
-                    case "A":
-                        MenuEmployee.ShowEmployeeMenu();
-                        break;
-                    case "Q":
-                        Console.WriteLine("Avslutar programmet...");
-                        return;
-                    default:
-                        Console.WriteLine("Felaktig inmatning. Välj antingen G, A eller Q.");
-                        break;
-                }
-            }
-        }
-
         // Metod för att låta användaren välja en gästprofil
         public static void ChooseGuestProfile()
         {
