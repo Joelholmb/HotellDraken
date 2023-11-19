8ec38e4dbc98c280ae74c0db2ec67996974cc5ce
Joel Holmberg <joel.holmberg@student.nbi-handelsakademin.se>
2023-11-19 14:47:17 +0100

    slagit ihop klasserna

--- Employees.cs
+++ Employees.cs
@@ -12,6 +12,21 @@ namespace hotelcsharp
             Username = username; //Ställ in egenskapen Användarnamn med det angivna användarnamnet
             Password = password; //Ställ in lösenordsegenskapen med det angivna lösenordet
         }
+    }
 
+        public class EmployeeList
+    {
+        private static List<Employee> employees = new List<Employee>
+        {
+            new Employee("liam", "admin123"),
+            new Employee("william", "admin000"),
+            new Employee("camilla", "admin999"),
+        };
+
+        public static bool Authenticate(string username, string password)
+        {
+            Employee employee = employees.Find(emp => emp.Username == username && emp.Password == password);
+            return employee != null;
+        }
     }
 }
--- Guests.cs
+++ Guests.cs
@@ -18,4 +18,15 @@ namespace hotelcsharp
             Email = email;
         }
     }
-}    
+
+        public class GuestList
+    {
+        public static List<Guest> guests = new List<Guest>
+        {
+        new Guest("Alfons", "Glassgatan 12", "0733111111", "inget@harjag.com"),
+        new Guest("Laban", "Melonvägen 5", "07332222222", "ett@jaghar.com"),
+        new Guest("Professor Baltazar", "Rödferrarigatan 45", "0733333333", "jaghartroligtvisett@trottsallt.com"),
+        };
+    }
+}
+    
